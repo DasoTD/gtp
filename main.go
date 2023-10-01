7a27d74833830ae39aa0b836c1e039de7257f946
David <daso.timileyin@gmail.com>
2023-10-01 14:38:51 +0100

    for correction

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main(){
+	server := socketio.NewServer(nil)
+
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+}
